f2c518bb4dfda7eceb7a4ea878db6452db8c9f47
Amman Corbin <elnovio.amie@outlook.com>
2019-12-19 08:36:04 +0530

    added and purhsed

---
+++ BookListRazor/Pages/BooksList/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor
+{
+    
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IndexModel(ApplicationDbContext db)
+        {
+
+            _db = db;
+        }
+        public List<Books> books { set; get; }
+        public async Task OnGet()
+        {
+            books = await _db.book.ToListAsync();
+        }
+        
+
+        
+    }
+}
